d333eff3bdf6f492a0eb7651eb84abbf5f2f27eb
jiangtt18 <tjiang90111@gmail.com>
2018-04-21 21:13:49 -0700

    finish passport and add nodemon

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require('express'); // import express
+const express = require('express');
+// import express
 const app = express(); // generate new application to configure different routes
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const googleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 // app.get('/', (req, res) => {
 // 	res.send({ hi: 'there' });
@@ -13,14 +14,18 @@ passport.use(
 	// client ID:   like session token
 	// client secret: like password digest
 	// create in keys.js
-	new GoogleStrategy(
+	new googleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback' // after user grant permission, redirect to this URL.
 		},
-		accessToken => {
-			console.log(accessToken);
+
+		// see terminal for below details
+		(accessToken, refreshToken, profile, done) => {
+			console.log('accessToken:', accessToken);// prove that user allows us to do sth with email/profile from google
+			console.log('refreshToken:', refreshToken);// allows us to refresh token
+			console.log('profile:', profile);
 		}
 	)
 );
@@ -31,6 +36,11 @@ app.get(
 	passport.authenticate('google', { scope: ['profile', 'email'] })
 	// scope values are google specific wording.
 );
+
+// after redirect, passport will see the code in the URL and sent them back to profile page
+// googleStrategy will handle the scope in the passport
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; // for deplying heroku by derfault use 5000
 app.listen(PORT);
